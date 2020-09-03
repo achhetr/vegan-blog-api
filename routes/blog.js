fec1dccb78e02dcb20d4ffb13820de22fc505e8c
Akash Chhetri <akash.chhetri1@gmail.com>
2020-09-03 19:09:26 +1000

    put method for blog

--- routes/blog.js
+++ routes/blog.js
@@ -29,10 +29,32 @@ route.post('/', auth, async (req,res) => {
         body: req.body.body,
         title: req.body.title
     }); 
-    let check = await blog.save();
-    console.log(check, 'checking');
-    res.send(check);
+    res.send(await blog.save());
 });
+
+route.put('/:blogId', auth, async (req,res) => {
+    const {error} = validateBlog(req.body);
+    if(error) return res.status(400).send(error.details[0].message); 
+    
+    let user = await User.findById(req.body.userId)
+    if(!user) return res.status(400).send('Invalid User');
+
+    let blog = await Blog.findById(req.params.blogId);
+    if(!blog) return res.status(400).send('Invalid Blog Id');
+
+    blog = await Blog.findByIdAndUpdate(req.params.blogId, {
+        user: user,
+        _id: blog._id,
+        body: req.body.body,
+        title: req.body.title
+    }, {new: true});
+
+    blog = await blog.save();
+
+    res.send(blog);
+});
+
+
 
 module.exports = route;
 
